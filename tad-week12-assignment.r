d6310a5cdd4778f52442afcf4bc07a7e4991cc90
Jess Spayd <89320923+jess-spayd@users.noreply.github.com>
2023-04-26 17:11:56 -0400

    Kaggle Notebook | TAD-week12-assignment | Version 1

---
+++ tad-week12-assignment.r
@@ -0,0 +1,147 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "3b307aaa",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002744,
+     "end_time": "2023-04-26T21:11:45.279772",
+     "exception": false,
+     "start_time": "2023-04-26T21:11:45.277028",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "1) Describe how each of the following metrics can be used to assess a Naive Bayes or other types of classifiers:\n",
+    "\n",
+    "- Accuracy\n",
+    "\n",
+    "- Precision\n",
+    "\n",
+    "- Recall\n",
+    "\n",
+    "2) Give an example for each of the three metrics above where you believe one would be better suited than the other three.\n",
+    "\n",
+    "3) Calculate accuracy, precision and recall for your Week 7 or 9 (Naive Bayes or SVM) homework.\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "55b01d1d",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001515,
+     "end_time": "2023-04-26T21:11:45.283250",
+     "exception": false,
+     "start_time": "2023-04-26T21:11:45.281735",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "692f4754",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001441,
+     "end_time": "2023-04-26T21:11:45.286265",
+     "exception": false,
+     "start_time": "2023-04-26T21:11:45.284824",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "\n",
+    "4) Vary input data in at least three ways  (Eg: Drop/don't drop stop words, keep only common words, label more data) and compute accuracy, precision and recall each time. What worked the best? Why do you think it worked better?\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "13daa59e",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001487,
+     "end_time": "2023-04-26T21:11:45.289282",
+     "exception": false,
+     "start_time": "2023-04-26T21:11:45.287795",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "id": "1d71b68d",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001448,
+     "end_time": "2023-04-26T21:11:45.292267",
+     "exception": false,
+     "start_time": "2023-04-26T21:11:45.290819",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "5) Super Bonus Question: Create and explain a confusion matrix for one of your model results."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "630f4177",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001515,
+     "end_time": "2023-04-26T21:11:45.295281",
+     "exception": false,
+     "start_time": "2023-04-26T21:11:45.293766",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.436237,
+   "end_time": "2023-04-26T21:11:45.418606",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2023-04-26T21:11:41.982369",
+   "version": "2.4.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
